8880d8138bb8c76998593edb86d6d53288834d3d
Quark <gennadiy.kaydalov@mail.ru>
2026-01-25 21:25:55 +0300

    hotfix

--- app/src/main/java/com/example/questionnaire/components/FloatingButtons.kt
+++ app/src/main/java/com/example/questionnaire/components/FloatingButtons.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.ArrowForward
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -36,6 +40,35 @@ fun FilePickerButton(launcher: ManagedActivityResultLauncher<Array<String>, Uri?
             Icon(
                 imageVector = Icons.Rounded.Add,
                 contentDescription = "Add",
+                tint = appColors.thirdBackground,
+                modifier = Modifier
+                    .fillMaxSize(0.7f)
+            )
+        }
+    }
+}
+
+@Composable
+fun IconButton(
+    onClickAction: () -> Unit
+){
+    val appColors = LocalAppColors.current
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        FloatingActionButton(
+            onClick = onClickAction,
+            shape = RoundedCornerShape(20.dp),
+            containerColor = appColors.secondaryBackground,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(26.dp)
+                .size(60.dp)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                contentDescription = "Back",
                 tint = appColors.thirdBackground,
                 modifier = Modifier
                     .fillMaxSize(0.7f)
--- app/src/main/java/com/example/questionnaire/components/QuestionCard.kt
+++ app/src/main/java/com/example/questionnaire/components/QuestionCard.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.example.questionnaire.viewers.LocalAppColors
 import com.example.questionnaire.models.QuestionModel
+import com.example.questionnaire.models.QuestionResult
 
 @Composable
 fun QuestionCard(
@@ -72,6 +73,91 @@ fun QuestionCard(
                         .fillMaxWidth()
                         .background(
                             color = if (isCorrect) appColors.highlights else Color.Transparent,
+                            shape = RoundedCornerShape(4.dp)
+                        )
+                        .padding(horizontal = 6.dp)
+                ) {
+                    Text(
+                        text = "${index + 1}. $answer",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = appColors.black
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun ResultQuestionCard(
+    questionIndex: Int,
+    questionItem: QuestionResult
+) {
+    val appColors = LocalAppColors.current
+
+    val question = questionItem.questionItem.question
+    val answers = questionItem.questionItem.answers
+    val correctAnswer = questionItem.questionItem.correctAnswerIndex
+    val selectedAnswer = questionItem.selectedAnswer
+
+    val isUnanswered = selectedAnswer == null
+
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(
+                color = when {
+                    !isUnanswered -> appColors.thirdBackground
+                    else -> Color.Gray.copy(0.6f)
+                }
+            )
+    ) {
+        //Question
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .dropShadow(
+                    shape = RoundedCornerShape(10.dp),
+                    shadow = Shadow(
+                        radius = 8.dp,
+                        spread = 0.dp,
+                        color = Color(0x30000000),
+                        offset = DpOffset(x = 0.dp, 2.dp)
+                    )
+                )
+                .background(appColors.secondaryBackground)
+                .padding(horizontal = 16.dp)
+                .padding(vertical = 10.dp)
+        ) {
+            Text(
+                text = "${questionIndex + 1}. $question",
+                style = MaterialTheme.typography.titleMedium,
+                color = appColors.whiteText
+            )
+        }
+        //Answers
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+        ){
+            Spacer(modifier = Modifier.height(4.dp))
+
+            answers.forEachIndexed { index, answer ->
+                val isCorrectAnswer = index == correctAnswer
+                val isSelectedAnswer = index == selectedAnswer
+
+                //Text highlight
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = when {
+                                !isUnanswered && isCorrectAnswer -> appColors.green
+                                !isUnanswered && isSelectedAnswer -> appColors.red
+                                else -> Color.Transparent
+                            },
                             shape = RoundedCornerShape(4.dp)
                         )
                         .padding(horizontal = 6.dp)
